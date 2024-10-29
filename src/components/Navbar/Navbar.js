2bd06345a5440e4c4589a691c12426c2c644fd5b
GuilhQueiroz <guiguimaraes.dev@gmail.com>
2024-10-29 14:24:45 -0300

    Ajustando a responsividade do site

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="nav-links">
+      <div className="menu-icon" onClick={toggleMenu} aria-expanded={isOpen} role="button" tabIndex={0} onKeyPress={(e) => e.key === 'Enter' && toggleMenu()}>
+        ☰
+      </div>
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
         <li><a href="#">Sobre nós</a></li>
         <li><a href="#">Contato</a></li>
         <li><a href="#" className="highlight">Por que apoiar?</a></li>
